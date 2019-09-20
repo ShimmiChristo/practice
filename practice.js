2c4cfb74ca8031e097d3cf245d50ef413bddafd2
Chris Shimmin <Chris@Chris-iMac.local>
2019-09-20 17:54:09 -0400

    5 intereview questions:

--- practice.js
+++ practice.js
@@ -52,11 +52,52 @@ function palidrome(string) {
 palidrome('carac');
 
 //How do you create a private variable in JS? -- Using Scope
-
 function secretVar() {
   var private = 'super secret code';
   return function() {
     return private;
   };
 }
-console.log(secretVar);
+var getPassword = secretVar();
+console.log(secretVar());
+console.log(getPassword());
+
+function Global() {
+  var foo = 'this is foo';
+  this.local = () => {
+    return 'this is local';
+  };
+}
+
+var getFoo = new Global();
+console.log(getFoo.local());
+
+//  What is the output of this variable?
+var num = 4;
+function outer() {
+  var num = 2;
+  function inner() {
+    num++;
+    var num = 3;
+    console.log(num);
+  }
+  inner();
+}
+outer();
+
+// What is the output?
+console.log(typeof typeof 1); // string
+
+// What is the output?
+
+var hero = {
+  _name: 'John Doe',
+  getSecretIdentity: function() {
+    return this._name;
+  }
+};
+// var stoleSecretIdentity = hero.getSecretIdentity;
+var stoleSecretIdentity = hero.getSecretIdentity.bind(hero);
+
+console.log(stoleSecretIdentity()); // undefined
+console.log(hero.getSecretIdentity()); // John Doe
