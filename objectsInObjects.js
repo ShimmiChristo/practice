9428a4f36b2a12fbd309810630f045ceec17da96
Chris Shimmin <Chris@Chris-iMac.local>
2019-10-02 12:39:26 -0400

    working on nested objects

--- objectsInObjects.js
+++ objectsInObjects.js
@@ -5,6 +5,25 @@ var list = [
   { id: 'c', state: 'MI', population: 200 },
   { id: 'c', state: 'MI', population: 100 }
 ];
+// var foo = { id: 'a', state: 'NJ', population: 150 };
+// var bar = Object.keys(foo).reduce(
+//   (obj, key) => Object.assign({}, obj, { [foo[key]]: key }),
+//   {}
+// );
+// console.log(bar);
+
+var obj = {};
+var bar = list.reduce((accumulator, currentVal, currentIndex) => {
+  // obj[i.id] = Object.assign({}, total, { [list[i].state] :  });
+  // obj[currentVal].id = Object.assign({}, obj, );
+  Object.keys(currentVal).forEach(key => {
+    accumulator[key] = (accumulator[key] || 0) + currentVal[key];
+  });
+  return accumulator;
+}, {});
+
+console.log(bar);
+
 // function compound(list) {
 function helper(obects) {
   // for (var i = 0; i < obects.length; i++) {
@@ -34,7 +53,7 @@ console.log(helper(list));
 //     3: { MI: 2, population: 300 }
 // }
 
-// Object.assign
+// Object.assign(target, ... values)
 // - method is used to copy the values of all enumerable own properties from one or more source objects to a target object. It will return the target object.
 const target = { a: 1, b: 2 };
 const source = { b: 4, c: 5 };
@@ -48,7 +67,7 @@ var data3 = {
       ingredients: [
         { name: 'tomato', value: 2 },
         { name: 'chicken', value: 3 },
-        { name: 'rice', value: 1 }
+        { name: 'rice', value: 0 }
       ]
     },
     {
@@ -77,3 +96,37 @@ console.log(ingredients);
 //   "rice": 1,
 //   "peas": 1
 // }
+
+// .reduce(acc, val, i)
+var list = [
+  { id: 'a', population: 150 },
+  { id: 'a', population: 200 },
+  { id: 'b', population: 500 },
+  { id: 'c', population: 200 },
+  { id: 'c', population: 100 }
+];
+
+// forEach
+var obj = {};
+list.forEach(val => {
+  var newObj = {};
+  if (!obj[val.id]) {
+    obj[val.id] = Object.assign({});
+    // obj[val.id] = newObj[((val.id = 1), val.population)];
+  } else {
+    obj[val.id] = obj[val.id] + val.population;
+  }
+  // console.log(obj)
+});
+console.log(obj);
+
+// reduce
+var res = list.reduce((total, val) => {
+  if (!total[val.id]) {
+    total[val.id] = val.population;
+  } else {
+    total[val.id] = total[val.id] + val.population;
+  }
+  return total;
+}, {});
+console.log(res);
