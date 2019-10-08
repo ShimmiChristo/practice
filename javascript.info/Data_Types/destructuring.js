44a453504be981b080f5e211858ab355380c26f1
Chris Shimmin <Chris@Chris-iMac.local>
2019-10-08 11:16:27 -0400

    destructuring

--- javascript.info/Data_Types/destructuring.js
+++ javascript.info/Data_Types/destructuring.js
@@ -29,3 +29,47 @@ for (let [key, value] of Object.entries(user)) {
   console.log(`${key}: ${value}`);
 }
 console.log(Object.entries(user));
+
+//* Destructuring Assignment
+let user = {
+  name: 'John',
+  years: 30
+};
+let { name, years: age, isAdmin = false } = user;
+console.log(name);
+console.log(age);
+console.log(isAdmin);
+
+//* The maximal salary
+let salaries = {
+  Pete: 300,
+  Mary: 250,
+  John: 100,
+  Chris: 300
+};
+function topSalary(salaries) {
+  var topPaid;
+  var currentPay = 0;
+  for (let [person, pay] of Object.entries(salaries)) {
+    if (pay > currentPay) {
+      currentPay = pay;
+      topPaid = person;
+    }
+  }
+  return topPaid || null;
+}
+topSalary(salaries);
+
+// Version 2
+function topSalary(salaries) {
+  let max = 0;
+  let maxName = null;
+  for (const [name, salary] of Object.entries(salaries)) {
+    if (max < salary) {
+      max = salary;
+      maxName = name;
+    }
+  }
+  return maxName;
+}
+topSalary(salaries);
