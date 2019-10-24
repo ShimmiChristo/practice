0f582589a5085e06c12b28dffa3d4474161af4c9
Chris Shimmin <Chris@Chris-iMac.local>
2019-10-24 11:25:03 -0400

    everything

--- eloquentJavascript/05_HigherOrderFunctions.js
+++ eloquentJavascript/05_HigherOrderFunctions.js
@@ -49,3 +49,41 @@ function loop(value, test, body, update) {
 
 loop(3, n => n > 0, n => n - 1, console.log);
 loop(-1, n => n > 0, n => n - 1, console.log);
+
+//! EVERYTHING
+/*
+Implement every as a function that takes an array and a predicate function as
+parameters. 
+Write two versions, one using a loop and one using the some method. 
+
+Every - De Morgan's Law 
+a && b equals !(!a || !b)
+all elements in the array match if there no element in the array that does not match
+*/
+function every(array, test) {
+  return !array.some(element => !test(element));
+}
+console.log(every([2, 4, 16], n => n < 10)); // false
+console.log(every([1, 3, 5], n => n < 10)); // true
+console.log(every([], n => n < 10)); // true
+
+function every(array, test) {
+  for (var element of array) {
+    if (!test(element)) return false;
+  }
+  return true;
+}
+console.log(every([2, 4, 16], n => n < 10)); // false
+console.log(every([1, 3, 5], n => n < 10)); // true
+console.log(every([], n => n < 10)); // true
+
+//! DOMINANT WRITING DIRECTION
+/*
+
+*/
+
+function dominantDirection(text) {
+  // code here
+}
+console.log(dominantDirection('Hello!')); // ltr
+console.log(dominantDirection('Hey, مساء الخير')); // rtl
