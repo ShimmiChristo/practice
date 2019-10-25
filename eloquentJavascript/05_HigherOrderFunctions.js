e23a499b56227d84885b931ce5aec6ba022f7ea9
Chris Shimmin <Chris@Chris-iMac.local>
2019-10-25 15:49:55 -0400

    chapter 5 solutions

--- eloquentJavascript/05_HigherOrderFunctions.js
+++ eloquentJavascript/05_HigherOrderFunctions.js
@@ -7,6 +7,7 @@
 - findIndex()
 
 */
+import { SCRIPTS } from './SCRIPTS/SCRIPTS.js';
 
 //! FLATTENING
 /*
@@ -79,11 +80,61 @@ console.log(every([], n => n < 10)); // true
 
 //! DOMINANT WRITING DIRECTION
 /*
+Write a function that computes the dominant writing direction in a string of text. 
+Remember that each script object has a direction property that can be "ltr" (left to right), "rtl" (right to left), or "ttb" (top to bottom).
 
+The dominant direction is the direction of a majority of the characters that have a script associated with them. 
+The characterScript and countBy functions defined earlier in the chapter are probably useful here.
 */
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (
+      script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })
+    ) {
+      return script;
+    }
+  }
+  return null;
+}
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({ name, count: 1 });
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+function dominantDirection(text) {
+  let counted = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : 'none';
+  }).filter(({ name }) => name != 'none');
+
+  if (counted.length == 0) return 'ltr';
+
+  return counted.reduce((a, b) => (a.count > b.count ? a : b)).name;
+}
+console.log(dominantDirection('Hello!')); // ltr
+console.log(dominantDirection('Hey, مساء الخير')); // rtl
 
 function dominantDirection(text) {
-  // code here
+  // let scripts = countBy(text, char => {
+  //   let script = characterScript(char.codePointAt(0));
+  //   return script ? script.name : 'none';
+  // });
+  // console.log(scripts);
+
+  // return characterScript(text.codePointAt(0));
+  for (var i = 0; i < text.length; i++) {
+    console.log('text i ' + text[i] + ' : ' + text.codePointAt(i));
+  }
 }
 console.log(dominantDirection('Hello!')); // ltr
 console.log(dominantDirection('Hey, مساء الخير')); // rtl
