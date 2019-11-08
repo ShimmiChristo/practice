a2d58286665f4e8914779aa80436bcb828dd40ef
Chris Shimmin <Chris@Chris-iMac.local>
2019-11-08 10:05:35 -0500

    working on creating the largest palindrome

--- freecodecamp/project_euler/04_largestPalindrome.js
+++ freecodecamp/project_euler/04_largestPalindrome.js
@@ -4,7 +4,13 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two n-digit numbers.
 */
 
-function largestPalindromeProduct(n) {}
+function largestPalindromeProduct(n) {
+  const maxPalindrome = function() {};
+  for (var i = 0; i <= 9; i++) {
+    console.log(i.toString().repeat(n));
+    // console.log(leng);
+  }
+}
 
 largestPalindromeProduct(2); // 9009
 largestPalindromeProduct(3); // 906609
