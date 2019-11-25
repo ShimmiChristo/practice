415ce89176011d9f73cb3adba323291e5e329d8d
shimmiChristo <shimmin.christopher@gmail.com>
2019-11-25 07:37:13 -0500

    recursion

--- javascript.info/part_1/06.01_recursion_stack.js
+++ javascript.info/part_1/06.01_recursion_stack.js
@@ -107,7 +107,24 @@ P.S. Hint: n! can be written as n * (n-1)! For instance: 3! = 3*2! = 3*2*1! = 6 
 
 function factorial(n) {}
 
-console.log(factorial(5)); // 120
+// console.log(factorial(5)); // 120
+
+//! 3. Fibonacci numbers
+/* The sequence of Fibonacci numbers has the formula Fn = Fn-1 + Fn-2. In other words, the next number is a sum of the two preceding ones.
+
+First two numbers are 1, then 2(1+1), then 3(1+2), 5(2+3) and so on: 1, 1, 2, 3, 5, 8, 13, 21....
+
+Fibonacci numbers are related to the Golden ratio and many natural phenomena around us.
+
+Write a function fib(n) that returns the n-th Fibonacci number. */
+
+function fib(n) {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+console.log(fib(7)); // 2
+// console.log(fib(7)); // 13
+// console.log(fib(77)); // 5527939700884757
 
 // ! Create a list tree
 let categories = [
@@ -121,8 +138,18 @@ let categories = [
   { id: 'siamese', parent: 'cats' }
 ];
 
+const listTree = (categories, parent) => {
+  let node = {};
+  categories
+    .filter(c => c.parent === parent)
+    .forEach(c => (node[c.id] = listTree(categories, c.id)));
+  return node;
+};
+
+// console.log(JSON.stringify(listTree(categories, null), null, 2));
+
 /* end goal destination
-{
+
   animals: {
     mammals: {
       dogs: {
