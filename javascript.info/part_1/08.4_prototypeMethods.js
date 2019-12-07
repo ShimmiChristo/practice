5a8a5a9a3cf90628f0c032ffc1935eeebf311fcd
shimmiChristo <shimmin.christopher@gmail.com>
2019-12-07 16:27:09 -0500

    something

--- javascript.info/part_1/08.4_prototypeMethods.js
+++ javascript.info/part_1/08.4_prototypeMethods.js
@@ -16,3 +16,4 @@ let rabbit = Object.create(animal);
 console.log(rabbit.eats); // true
 console.log(Object.getPrototypeOf(rabbit) === animal); // true
 Object.setPrototypeOf(rabbit, {}); // change the prototype of rabbit to {}
+
